dbb55bd8fa02f2369b7123c31924fb015660d5b2
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-03-12 15:44:41 +0800

    ci image

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"github.com/weizhoublue/github-action-test/pkg/lock"
 	"github.com/weizhoublue/github-action-test/pkg/print"
+	"time"
 )
+
+func TestRace() {
+	a := 10
+
+	fmt.Println("TestRace ")
+	go func() {
+		a++
+	}()
+	go func() {
+		a++
+	}()
+	time.Sleep(2 * time.Second)
+
+}
+
+func Testlock() {
+	fmt.Println("Testlock ")
+	a := &lock.RWMutex{}
+	a.RLock()
+	a.Lock()
+	time.Sleep(10 * time.Second)
+	a.Unlock()
+	return
+}
 
 func main() {
 	fmt.Println("hello world")
+	TestRace()
+	Testlock()
 	print.MyPrint()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/weizhoublue/github-action-test
 
 go 1.17
+
+require github.com/sasha-s/go-deadlock v0.3.1
+
+require github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
