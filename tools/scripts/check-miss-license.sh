a4417b2a0f7675d0b9c6c337229e636b8a899619
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-03-20 23:47:10 +0800

    dsaf

--- tools/scripts/check-miss-license.sh
+++ tools/scripts/check-miss-license.sh
@@ -6,7 +6,7 @@ set -o pipefail
 CURRENT_DIR_PATH=$(cd `dirname $0`; pwd)
 PROJECT_ROOT_PATH="${CURRENT_DIR_PATH}/../.."
 
-FILE_LIST=$( egrep -i "Apache-2.0|Copyright"  ${PROJECT_ROOT_PATH}/*  --exclude-dir=./vendor  -RHL --include=*.go --include=*.sh  2>/dev/null )
+FILE_LIST=$( egrep -i "Apache-2.0|Copyright"  ${PROJECT_ROOT_PATH}/*  --exclude-dir=${PROJECT_ROOT_PATH}/vendor  -RHL --include=*.go --include=*.sh  2>/dev/null )
 (( $? != 0 )) && echo "error, failed to grep " && exit 1
 if [ -n "$FILE_LIST" ]; then
     echo "error, found go file who missing licecse announce "
